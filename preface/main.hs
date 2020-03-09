af1b7007e80f4b65015d101783597d44e49fd990
Jan Vandepitte <Jan.Vandepitte@sirus.be>
2020-03-09 22:01:23 +0100

    preface

--- preface/main.hs
+++ preface/main.hs
@@ -1,1 +1,9 @@
-main = putStrLn "Hello Haskell!"
+main = do
+    putStrLn "Hello Haskell!"
+    putStrLn "This is a prime number"
+    print (2^31 -1)
+
+id :: a -> a -- identity fuction
+id a = a
+
+{-# language ExplicitForAll #-}
